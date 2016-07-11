ce6932a2211b39c922e61b7b522dc6caf94c0155
Matt Bostock <matt@mattbostock.com>
2016-07-11 22:30:20 +0100

    Define navigation timings explicitly
    Previously, we unmarshalled the return value of `performance.timings` as
    a map of floats, which was rather brittle.
    
    It seems some (more recent?) versions of Chrome include a `toJSON`
    function as part of the `performance.timings` return value, which caused
    unmarshaling to fail:
    
        ERRO[0036] failed to run script: unexpected response:
        {"sessionId":"acc66d616b5043208ed9d91c0e1084b4","status":0,"value":{"connectEnd":1468266125271,"connectStart":1468266125271,"domComplete":1468266126349,"domContentLoadedEventEnd":1468266125856,"domContentLoadedEventStart":1468266125701,"domInteractive":1468266125700,"domLoading":1468266125491,"domainLookupEnd":1468266125271,"domainLookupStart":1468266125271,"fetchStart":1468266125271,"loadEventEnd":1468266126350,"loadEventStart":1468266126349,"navigationStart":1468266125039,"redirectEnd":0,"redirectStart":0,"requestStart":1468266125293,"responseEnd":1468266125489,"responseStart":1468266125488,"secureConnectionStart":0,"toJSON":{},"unloadEventEnd":0,"unloadEventStart":0}}
        source=probe.go:56
    
    Instead, be explicit about the exact values we want to parse and using a
    struct and JSON tags.
    
    This also removes the `snakeCase()` function which I wasn't keen on.
    
    Fixes #1.

--- probe.go
+++ probe.go
@@ -16,11 +16,37 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"unicode"
 
 	"github.com/prometheus/common/log"
 	"github.com/sclevine/agouti"
 )
+
+const msInSecond = 1000
+
+// from https://www.w3.org/TR/navigation-timing/#sec-navigation-timing-interface
+type navigationTimings struct {
+	ConnectEnd                 float64 `json:"connectEnd"`
+	ConnectStart               float64 `json:"connectStart"`
+	DomComplete                float64 `json:"domComplete"`
+	DomContentLoadedEventEnd   float64 `json:"domContentLoadedEventEnd"`
+	DomContentLoadedEventStart float64 `json:"domContentLoadedEventStart"`
+	DomInteractive             float64 `json:"domInteractive"`
+	DomLoading                 float64 `json:"domLoading"`
+	DomainLookupEnd            float64 `json:"domainLookupEnd"`
+	DomainLookupStart          float64 `json:"domainLookupStart"`
+	FetchStart                 float64 `json:"fetchStart"`
+	LoadEventEnd               float64 `json:"loadEventEnd"`
+	LoadEventStart             float64 `json:"loadEventStart"`
+	NavigationStart            float64 `json:"navigationStart"`
+	RedirectEnd                float64 `json:"redirectEnd"`
+	RedirectStart              float64 `json:"redirectStart"`
+	RequestStart               float64 `json:"requestStart"`
+	ResponseEnd                float64 `json:"responseEnd"`
+	ResponseStart              float64 `json:"responseStart"`
+	SecureConnectionStart      float64 `json:"secureConnectionStart"`
+	UnloadEventEnd             float64 `json:"unloadEventEnd"`
+	UnloadEventStart           float64 `json:"unloadEventStart"`
+}
 
 func probe(target string, w http.ResponseWriter) bool {
 	// new page to ensure a clean session (no caching)
@@ -50,15 +76,34 @@ func probe(target string, w http.ResponseWriter) bool {
 		return false
 	}
 
-	var timings map[string]float64
+	var timings *navigationTimings
 	err = page.RunScript("return window.performance.timing;", nil, &timings)
 	if err != nil {
 		log.Error(err)
 		return false
 	}
-	for k, v := range timings {
-		fmt.Fprintf(w, "navigation_timing_%s_seconds %f\n", snakeCase(k), v/1000)
-	}
+
+	fmt.Fprintf(w, "navigation_timing_connect_end_seconds %f\n", timings.ConnectEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_connect_start_seconds %f\n", timings.ConnectStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_dom_complete_seconds %f\n", timings.DomComplete/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_dom_content_loaded_event_end_seconds %f\n", timings.DomContentLoadedEventEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_dom_content_loaded_event_start_seconds %f\n", timings.DomContentLoadedEventStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_dom_interactive_seconds %f\n", timings.DomInteractive/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_dom_loading_seconds %f\n", timings.DomLoading/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_domain_lookup_end_seconds %f\n", timings.DomainLookupEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_domain_lookup_start_seconds %f\n", timings.DomainLookupStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_fetch_start_seconds %f\n", timings.FetchStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_load_event_end_seconds %f\n", timings.LoadEventEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_load_event_start_seconds %f\n", timings.LoadEventStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_redirect_end_seconds %f\n", timings.RedirectEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_redirect_start_seconds %f\n", timings.RedirectStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_request_start_seconds %f\n", timings.RequestStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_response_end_seconds %f\n", timings.ResponseEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_response_start_seconds %f\n", timings.ResponseStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_secure_connection_start_seconds %f\n", timings.SecureConnectionStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_start_seconds %f\n", timings.NavigationStart/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_unload_event_end_seconds %f\n", timings.UnloadEventEnd/msInSecond)
+	fmt.Fprintf(w, "navigation_timing_unload_event_start_seconds %f\n", timings.UnloadEventStart/msInSecond)
 
 	logs, err := page.ReadAllLogs("browser")
 	if err != nil {
@@ -78,19 +123,4 @@ func probe(target string, w http.ResponseWriter) bool {
 	fmt.Fprintf(w, "browser_log_severe_count %d\n", severeCount)
 
 	return true
-}
-
-func snakeCase(in string) string {
-	runes := []rune(in)
-	length := len(runes)
-
-	var out []rune
-	for i := 0; i < length; i++ {
-		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
-			out = append(out, '_')
-		}
-		out = append(out, unicode.ToLower(runes[i]))
-	}
-
-	return string(out)
 }
